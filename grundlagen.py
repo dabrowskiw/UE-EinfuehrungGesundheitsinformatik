f524aa1a0b6df8722a3c5f5112ba81ff3f9e3ce4
dabrowskiw <falka777@gmail.com>
2019-09-18 23:10:29 +0200

    Update grundlagen.py

--- grundlagen.py
+++ grundlagen.py
@@ -24,3 +24,73 @@ print("y="+y)
 
 # Stattdessen muss man explizit konvertieren:
 print("y="+str(y))
+
+# Jede Variable in Python ist ein Objekt:
+print(x.bit_length())
+
+# Nützliche Funktion: dir() zeigt alle Methoden und Attribute eines Objektes an:
+dir(x)
+dir("   test")
+print("   test".upper())
+print("   test".strip())
+
+# Methoden mit __ sind "magic"-Methoden, nicht für direkte Verwendung gedacht
+
+# Zum formatieren von strings existiert eine Spezifikationssprache:
+# {} sind placeholder für Variablen
+print("Der Wert von y: {} minus den Wert von x: {} ist: {}".format(x, y, x-y))
+
+# Mit {} können auch Positionen in der Argumentenliste angegeben werden:
+print("y: {1}, x: {0}".format(x, y)) 
+
+# Positionen können wiederholt werden:
+print("{0}{1}{0}".format("abra", "cad")) 
+
+# Zahlenformate können definiert werden:
+print("723 in binary: {0:b} and hex: {0:X}".format(723)) 
+
+# Damit kann man übrigens schön die Ergebnisse binärer Operationen anschauen:
+print("1: {:b}, 1<<2: {:b}, 60: {:b}, ~60: {:b} (dezimal: {}), 13: {:b}, 60&13: {:b}, 60|13: {:b} etc...".format(1, 1<<2, 60, ~60, ~60, 13, 60&13, 60|13)) 
+# Frage: Kann jemand erklären, wieso ~60==-61
+
+# Man kann links, rechts oder mittig alignen und sich das Fuellzeichen aussuchen:
+print("y 10-stellig rechtsbündig: {0:>10}, linksbündig: {0:<10}, zentriert: {0:^10} und rechtsbündig mit 0: {0:0>10}".format(y))
+
+
+# String-delimiter können ", ', oder (für multiline) """ sein:
+print('Das ist z.B. nuetzlich um " nicht escapen zu muessen')
+
+dat="""Oder um
+mehrere Zeilen Text
+einfach einzugeben"""
+print(dat)
+
+# Es existieren viele convenience-Funktionen für strings, z.B.:
+
+dat2 = dat.replace("\n", " ")
+print(dat2)
+print(dat2.split(" "))
+
+# (das geht alles auch mit regexp, Modul re, aber das führt zu weit)
+
+# Split führt uns zu Datentypen. Neben den klassischen integer, float, string haben wir Arrays:
+x = [1, 2, 3, 4, 5, 6, 7, 8]
+print(x)
+
+# Vorsicht: Durch die dynamische Typisierung geht das x=[1,2,3] oben: x ist jetzt kein integer mehr, sondern ein Array!
+# Adressierung von Arrays wie gewohnt:
+print(x[0])
+print(dat2.split(" ")[3])
+
+# Man kann mit Arrays viele lustige Dinge tun, z.B.:
+# slicen
+print(x[2:5])
+print(x[5:]) 
+print(x[:5])
+
+# von hinten adressieren
+print(x[-3])
+
+print(x[1,3,4])
+
+# String-Konkatenation
